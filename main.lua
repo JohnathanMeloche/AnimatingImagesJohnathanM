2d733239e70ffd7922654e511ec3faed795ceb70
Johnathan Meloche <johnathan.meloche@ocsbstudent.ca>
2018-03-06 10:11:24 -0500

    Start

--- main.lua
+++ main.lua
@@ -1,7 +1,9 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- This program animates 3 different
+-- objects in different ways
+-- Johnathan M ICS20
 
--- Your code here
+-- change background colour
+display.setDefault ("background", 10/255, 30/255, 100/255)
+
+-- hide the status bar
+display.setStatusBar (display.HiddenStatusBar)
